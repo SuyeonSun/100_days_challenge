2bf5c5e2c406404da22cd4b652f1fb1da7606d23
SuyeonSun <suninisy@gmail.com>
2023-05-23 13:30:17 +0900

    불필요한 코드 제거

--- src/main/java/com/example/_days_challenge/ChallengeSaveRequestDto.java
+++ src/main/java/com/example/_days_challenge/ChallengeSaveRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@Getter // TODO: 왜 필요?
+@Getter
 public class ChallengeSaveRequestDto {
     private String title;
 
--- src/main/java/com/example/_days_challenge/entity/Challenge.java
+++ src/main/java/com/example/_days_challenge/entity/Challenge.java
@@ -5,12 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
-@Getter // TODO: 왜 필요?
 public class Challenge {
     @Id @GeneratedValue
     @Column(name = "challenge_id")
